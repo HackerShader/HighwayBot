2d8795232e2d3be8d10c2145ac8dcd9d1edfd374
VaitoSoi <duyduy15456@gmail.com>
2022-03-31 19:42:13 +0700

    Update mine.js

--- commands/mine.js
+++ commands/mine.js
@@ -60,35 +60,40 @@ module.exports = {
         }
         async function placeNetherrack(y, z) {
             const target = bot.blockAt(bot.entity.position.offset(3, y, z))
-          //  bot.inventory.slots.forEach(async (slot) => {
-                //console.log(slot.name)
-                //if (!slot || slot.name !== 'netherrack') return
-                await bot.equip(87, 'hand')
-                // console.log(target.position.plus(new Vec3(0, 1, 0)))
-                try {
-                    bot.placeBlock(target, new Vec3(0, 1, 0))    
-                } catch (error) {
-                    console.log(error.stack)
-                }
-           // })
+            //  bot.inventory.slots.forEach(async (slot) => {
+            //console.log(slot.name)
+            //if (!slot || slot.name !== 'netherrack') return
+            await bot.equip(87, 'hand')
+            // console.log(target.position.plus(new Vec3(0, 1, 0)))
+            try {
+                bot.placeBlock(target, new Vec3(0, 1, 0))
+            } catch (error) {
+                console.log(error.stack)
+            }
+            // })
         }
-        async function checkLava(y, z) {
-            let check = Boolean
-            const target1 = bot.blockAt(bot.entity.position.offset(2, y, z - 1))
-            const target2 = bot.blockAt(bot.entity.position.offset(2, y + 1, z))
-            const target3 = bot.blockAt(bot.entity.position.offset(2, y, z + 1))
-            const target4 = bot.blockAt(bot.entity.position.offset(2, y - 1, z))
-            if (!target1 && !target2 && !target3 && !target4) {
-                check = false
-                stop = true
-            } else {
-                if (target1.name === 'lava' || target2.name === 'lava' || target3.name === 'lava' || target4.name === 'lava') {
-                    check = true
-                } else if (target1.name !== 'lava' && target2.name !== 'lava' && target3.name !== 'lava' && target4.name !== 'lava') {
-                    check = false
+        async function check3wall() {
+            let array = []
+            for (var x = 2; x <= 5; x++) {
+                for (var y = 4; y >= -1; y--) {
+                    if (y != 0 && y != -1) {
+                        for (var z = -3; y <= 3; y++) {
+                            const target = bot.blockAt(bot.entity.position.offset(x, y ,z))
+                            if (target.name === 'lava') array.push(
+                                `${target.position.x} ${target.position.y} ${target.position.z}`
+                            )
+                        }
+                    } else if (y == 0 || y == -1) {
+                        for (var z = -1; y <= 1; y++) {
+                            const target = bot.blockAt(bot.entity.position.offset(x, y ,z))
+                            if (target.name === 'lava') array.push(
+                                `${target.position.x} ${target.position.y} ${target.position.z}`
+                            )
+                        }
+                    }
                 }
             }
-            return check
+            return array
         }
         /**
          * 
