4643ff4d5141366d8993f2ee26bc4333ea4afcf5
hackershader <thuybdtmc@gmail.com>
2022-04-20 00:03:50 +0700

    Build 04202022.0003

--- commands/inventory.js
+++ commands/inventory.js
@@ -29,26 +29,8 @@ module.exports = {
             }
         }
         if (args[0] == `echest`) {
-            for (let x = -1; x <= 1; x++) {
-                for (let z = -1; z <= 1; z++) {
-                    if (math.abs(x) === math.abs(z)) {
-                        continue;
-                    }
-                    const checkairblock = await bot.blockAt(bot.entity.position.offset(x, 0, z));
-                    console.log(checkairblock.name)
-                    if (checkairblock.name === 'air') {
-                        try {
-                            const target = bot.blockAt(bot.entity.position.offset(checkairblock.position.x, 0, checkairblock.position.z))
-                            console.log(`trying to place echest at ${x}, ${z}`)
-                            bot.equip(130, 'hand')
-                            await bot.placeBlock(target, new Vec3(1, 0, 0));
-                            break;
-                        } catch (err) {
-                            console.log(err)
-                        }
-                    }
-                }
-            }
+            //test
+
         }
     }
 }
