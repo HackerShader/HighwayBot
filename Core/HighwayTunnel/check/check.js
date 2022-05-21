6d9d5bc15f6732f00928a03b5d6008205fe8ed2e
hackershader <thuybdtmc@gmail.com>
2022-05-21 12:07:33 +0700

    Rewriting check.js (2)

--- Core/HighwayTunnel/check/check.js
+++ Core/HighwayTunnel/check/check.js
@@ -1,13 +1,16 @@
 module.exports = async (bot) => {
-    for (let x = 0; x <= 2; x++) {
-        for (let y = 0; y <= 3; y++) {
-            for (let z = -2; z == 2; z++) {
-                const target = bot.blockAt(bot.entity.position.offset(2, y, z))
-                if ((z === -2 || z === 2) && y === 0 && target) continue;
-                //if (target.name !== 'air') require('./../break/minerewrite')(bot)
-                console.log(target.name)
-                bot.navigate.to(bot.entity.position.offset(1, 0, 0));
+    //await require('./../break/minerewrite')(bot)
+    async function check() {
+        for (let x = 0; x <= 2; x++) {
+            for (let y = 0; y <= 3; y++) {
+                for (let z = -2; z <= 2; z++) {
+                    const target = bot.blockAt(bot.entity.position.offset(2, y, z))
+                    if ((z === -2 || z === 2) && y === 0 && target) continue;
+                    if (target.name !== 'air') await require('./../break/minerewrite')(bot)
+                    bot.navigate.to(bot.entity.position.offset(2, 0, 0));
+                }
             }
         }
     }
+    await check()
 }
