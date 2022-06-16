eff284561bcb6106fed02f8a026cb18998404164
HackerShader <72206549+HackerShader@users.noreply.github.com>
2022-06-16 22:20:55 +0700

    updating config.js

--- cmd/config.js
+++ cmd/config.js
@@ -7,8 +7,19 @@ module.exports = {
     async execute(args) {
         const package = require('./../package.json')
         //if (package.build === undefined) return console.log('[X] HighwayBot not installed]')
+        //check value exists in default.json
+        const defaultConfig = editJsonFile('./../config/default.json')
+        const defaultConfigValues = Object.values(defaultConfig.data)
+
+
         if (!fs.existsSync('./config')) fs.mkdirSync('./config')
-        if (!fs.existsSync('./config/default.json')) fs.writeFileSync('./config/defaultjson', '{\n}')
+        if (!fs.existsSync('./config/default.json')) fs.writeFileSync('./config/default.json', '{\n}')
         console.log(args[1])
+        console.log(defaultConfigValues)
+
+
+
+        
+
     }
 }
