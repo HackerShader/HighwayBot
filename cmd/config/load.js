e7408d2b691d6df884bacff4586a27d0e563071a
HackerShader <72206549+HackerShader@users.noreply.github.com>
2022-06-17 21:57:49 +0700

    Fixed load.js

--- cmd/config/load.js
+++ cmd/config/load.js
@@ -1,12 +1,11 @@
 const editJsonFile = require('edit-json-file')
 const fs = require('fs-extra')
 
-module.exports = async(args, args2) => {    
-    const commandconfig = editJsonFile(`./../../commandconfig.json`)
+module.exports = async (args, args2) => {
     if (!args) return console.log(`[Config | Load] Usage: config load <filename>`)
     if (!fs.existsSync(`./config/${args}.json`)) return console.log(`[Config | Load | Error] Config [${args}] does not exist`)
-    commandconfig.set(`config`, `./config/${args}.json`)
+    const commandconfig = editJsonFile(`./commandconfig.json`)
+    commandconfig.set('config', `./config/${args}.json`)
     commandconfig.save()
     return console.log(`[Config | Load | Done] Config [${args}] loaded`)
-
 }
