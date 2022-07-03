f9f9f2ee006c749fd76e7d56cab1e2b52baa578f
hackershader <thuybdtmc@gmail.com>
2022-07-03 08:41:51 +0700

    Reformat velocity

--- Core/Player/Movenment/velocity.js
+++ Core/Player/Movenment/velocity.js
@@ -1,8 +1,8 @@
 module.exports = async (bot) => {
     bot._client.on('entity_velocity', v => {
-        if (bot.entity.id !== v.entityId) return
-        bot.entity.velocity.x = 0
-        bot.entity.velocity.y = 0
-        bot.entity.velocity.z = 0
-    })
-}
+        if (bot.entity.id !== v.entityId) return;
+        bot.entity.velocity.x = 0;
+        bot.entity.velocity.y = 0;
+        bot.entity.velocity.z = 0;
+    });
+};
