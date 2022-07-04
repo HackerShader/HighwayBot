5a2cfe90ecad7a8f8c5d5e39b18a3d021727c3fe
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-07-04 22:07:21 +0700

    Fixed import numbers and output error

--- cmd/config/edit.js
+++ cmd/config/edit.js
@@ -4,6 +4,9 @@ module.exports = (args) => {
     if (!args[2]) return console.log(`[Config | Edit] Usage: config edit <filename> <key1>:<value1> <key2>:<value2>...`);
     if (!fs.existsSync(`./config/${args[2]}.json`)) return console.log(`\x1b[31m[Config | Edit | Error] Config [${args[2]}] don't exists\x1b[0m`);
     const file = require('edit-json-file')(`./config/${args[2]}.json`);
+    // đã gán giá trị ở đây nên ko xài được dòng 18
+    let configName = args[2];
+    let success = false;
     args.slice(3).forEach((args) => {
         let key, value, i = 0;
         args.split('').forEach((c) => {
@@ -12,23 +15,25 @@ module.exports = (args) => {
                 value = args.split('').splice(i + 1).join('');
             } else i++;
         });
-        if (!Object.keys(require('../../config/default.json')).includes(key))
-            console.log(`\x1b[31m[Config | Edit | Error] Key [${key}] don't exists\x1b[0m`);
+        if (!Object.keys(require(`../../config/${configName}.json`)).includes(key))
+            return console.log(`\x1b[31m[Config | Edit | Error] Key [${key}] don't exists\x1b[0m`);
         let number = [
             'port',
             'pin',
             'key',
         ];
-        if (number.includes(key)) {
-            let test = Number(value);
-            if (Number.isNaN(test))
+        if (number.indexOf(key) > -1) {
+            if (isNaN(value))
                 return console.log(`\x1b[31m[Config | Edit | Error] [${key}] key must be a number\x1b[0m`);
         }
         if (key === 'password' && value.toLowerCase() === 'null') value = null;
         if (key === 'version' && !require('../../Core/Game/version.json').includes(value))
             return console.log(`\x1b[31m[Config | Edit | Error] Invalid version (1.8 -> 1.18 only)\x1b[0m`);
-        file.set(key, value);
+        let value2 = parseInt(value);
+        if(!value2) value2 = value;
+        file.set(key, value2);
+        file.save();
+        success = true;
     });
-    file.save();
-    console.log(`\x1b[32m[Config | Edit | Done] Edited [${args[2]}] config.\x1b[0m`);
+    if(success) console.log(`\x1b[32m[Config | Edit | Done] Edited [${args[2]}] config.\x1b[0m`);
 };
