8fae8ba067833cf0846c0b35bc3fcc4e8c57439c
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-07-04 22:44:23 +0700

    Clean up & fix showing config

--- cmd/config/show.js
+++ cmd/config/show.js
@@ -1,11 +1,12 @@
 const fs = require('fs-extra');
 const ascii = require('ascii-table');
-const table = new ascii().setHeading('Keys', 'Values');
 
 module.exports = (args) => {
+    const table = new ascii().setHeading('Keys', 'Values');
     if (!args) return console.log(`[Config | Show] Usage: config show <filename>`);
     if (!fs.existsSync(`./config/${args}.json`)) return console.log(`\x1b[31m[Config | Clone | Error] Config [${args}] does not exist\x1b[0m`);
-    const file = require(`../../config/${args}`);
+    delete require.cache[require.resolve(`./../../config/${args}.json`)];
+    const file = require(`../../config/${args}.json`);
     Object.keys(file).forEach(key => {
         let value;
         if (file[key]) value = (file[key]).toString();
