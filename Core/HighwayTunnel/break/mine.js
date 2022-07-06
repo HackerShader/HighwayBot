4e4540461079fd765d96094f3dd651b2e0d63607
hackershader <thuybdtmc@gmail.com>
2022-07-06 07:44:47 +0700

    Reformat code (4)

--- Core/HighwayTunnel/break/mine.js
+++ Core/HighwayTunnel/break/mine.js
@@ -1,6 +1,6 @@
 let stop = Boolean;
 const Vec3 = require('vec3').Vec3;
-const config = require(`../../../config/${require('../../../path.json').config}`);
+//const config = require(`../../../config/${require('../../../path.json').config}`);
 const log = require('../../Console/log');
 const {Bot} = require('mineflayer');
 
@@ -14,15 +14,14 @@ module.exports = async (bot) => {
         for (let x = -3; x <= 2; x++) {
             for (let y = 3; y >= 0; y--) {
                 for (let z = -2; z <= 2; z++) {
-                    const target = bot.blockAt(bot.entity.position.offset(x, y, z)),
-                        pos = `${Math.round(bot.entity.position.x)} ${Math.round(bot.entity.position.y)} ${Math.round(bot.entity.position.z)}`
-                        , pos2 = `${target.position.x} ${target.position.y} ${target.position.z}`;
+                    const target = bot.blockAt(bot.entity.position.offset(x, y, z))
+                        , pos = `${target.position.x} ${target.position.y} ${target.position.z}`;
                     if (target.name === 'air' || !bot.canDigBlock(target) || !target) continue;
                     if ((z === -2 || z === 2) && y === 0 && target) continue;
-                    log(target.name, pos2, 'dig', true);
+                    log(target.name, pos, 'dig', true);
                     await bot.dig(target, true, new Vec3(-1, 0, 0));
                     //await bot.swingArm('right', true)
-                    log(target.name, pos2, 'done', true);
+                    log(target.name, pos, 'done', true);
                 }
             }
         }
