50a3d42f93e28a3665798d5060218f4cfdc7ae74
hackershader <72206549+HackerShader@users.noreply.github.com>
2022-08-14 15:13:04 +0700

    Pre release 081422

--- cmd/changelog.js
+++ cmd/changelog.js
@@ -6,8 +6,13 @@ module.exports = {
     aliases: ['updateinfo'],
     execute() {
         if (info.version === undefined && info.build === undefined) return console.log('\x1b[0m[X] HighwayBot not installed!\x1b[0m');
-        console.log(
-        `\x1b[0m[\x1b[32mHighwayBot Changelog\x1b[0m]\x1b[0m` 
-        );
+        console.log(`\x1b[0m[Notification] HighwayBot changelog ${info.version} ${info.build}\x1b[0m` +
+            '**Added/Improved:**\n' +
+            '\n' +
+            '> The Downloader of HighwayBot\n' +
+            '> Improved HighwayBot dig algorithm\n' +
+            '> Aliases command for CLI\n' +
+            '> Improved Config system\n' +
+            '> Added Velocity, AutoCrystal (might break), AutoTotem')
     }
 };
