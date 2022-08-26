8f4065e0c683a3e50814ef4a210c7542500106f2
VaitoSoi <duyduy15456@gmail.com>
2022-08-26 22:16:06 +0700

    Update translate tool (5)

--- language/vi.js
+++ language/vi.js
@@ -71,5 +71,83 @@ module.exports = {
          * @param {String} message Message
          */
         chat: (message) => `[MC-Bot | Tin nhắn] ${message}`
-    }
+    },
+    Core: {
+        Baritone: {
+            follow: {
+                cant_see: () => `Bot không tìm thấy bạn!`,
+                /**
+                 * @param {String} coord Coord
+                 */
+                follow: (coord) => `[Baritone | Đi theo] Tọa độ: ${coord}`
+            },
+            goto: {
+                err_miss_coord: () => `[Baritone | Lỗi | Đi đến] Thiếu tọa độ`,
+                err_invalid_y: () => `[Baritone | Lỗi | Giá trị] Giá trị Y không hợp lệ (0 < Y < 255)`,
+                /**
+                 * @param {String} coord Coord
+                 */
+                goto: (coord) => `[Baritone | Đi đến] Tọa độ: ${coord}`
+            }
+        },
+        Console: {
+            log: {
+                /**
+                 * @param {{name: String, pos: String, status: String}} info Status
+                 * @param {{mine: Number, place: Number, "place-err": Number, PickaxeBroken: Array}} data Data
+                 */
+                log: (info, data) =>
+                    `[HighwayBot] Thông tin\n` +
+                    `> Tên khối: ${info.name}\n` +
+                    `> Vị trí: ${info.pos}\n` +
+                    `> Tình trạng: ${info.status}\n` +
+                    `[HighwayBot] Tiến độ\n` +
+                    `> Tổng số khối đã đập: ${data.mine}\n` +
+                    `> Tổng số khối đã đặt: ${data.place}\n` +
+                    `> Tổng số lỗi: ${data["place-err"]}\n` +
+                    `[HighwayBot] Tình trạng cúp\n` +
+                    `> Độ bền đã dùng: ${data.Pickaxe1.durability}\n` +
+                    `> Tổng số cúp đã hư: ${(data["PickaxeBroken"]).length}`
+            }
+        },
+        HighwayTunnel: {
+            inventory: {
+                item_saver: {
+                    think: () => `Hmmm...Tôi không có cây cúp nào cả.`
+                }
+            },
+            highwaybuildtool: {
+                start: () => `[Highway] Bắt đầu quá trình đào`
+            }
+        }
+    },
+    commands: {
+        /**
+         * @param {String} commands Commands
+         */
+        no_command: (commands) => `[Baritone] Toàn bộ lệnh: ${commands}`,
+        /**
+         * @param {String} command Command
+         */
+        command_invalid: (command) => `[Baritone | Lỗi] Không tìm thấy lệnh ${command}`,
+        /**
+         * @param {String} error Error
+         */
+        command_err: (error) => `[Baritone | Lỗi] ${error}`
+    },
+    help: {
+        help: () => `Nếu bạn cần giúp đỡ, vui lòng vào cli và dùng lệnh 'help' hoặc truy cập 'https://highwaybot.tk/category/command'`
+    },
+    infoserver: {
+        /**
+         * @param {String} ip Server's IP
+         * @param {Number} tps TPS
+         * @param {Number} players Players
+         */
+        info: (ip, tps, players) => `[${ip}] TPS: ${tps} | Tổng số người chơi: ${players}`
+    },
+    mine: {
+        stop: () => `[Highway] Đã dừng việc đào`
+    },
+    reload: {}
 }
