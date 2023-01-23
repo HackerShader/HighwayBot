1507363df68afca8535cd56d32197d8d42abeeb5
VaitoSoi <duyduy15456@gmail.com>
2023-01-23 17:23:26 +0700

    Update Installer
    Remove 'yarn'

--- installer.js
+++ installer.js
@@ -1,38 +1,32 @@
-const yarn = (() => {
-    const child_process = require('child_process')
-    const list = child_process.execSync('npm list -g yarn').toString('utf8').split('\n').slice(1, -2)[0]
-    return /^(└──|`--) (.+)@(.+)$/.test(list)
-})()
-
 function install() {
-    const child_process = require('child_process')
-    const list = ['axios', 'unzipper', 'fs-extra']
-    process.stdout.write(`[-] Downloading ${list.length} package(s)`)
-    child_process.execSync(`${yarn == true ? 'yarn add' : 'npm install'} ${list.join(' ')}`)
-    process.stdout.clearLine()
-    process.stdout.cursorTo(0)
-    console.log('[#] Downloaded package(s)')
-    download()
-}
+    const child_process = require('child_process');
+    const list = ['axios', 'unzipper', 'fs-extra'];
+    process.stdout.write(`[-] Downloading ${list.length} package(s)`);
+    child_process.execSync(`npm install ${list.join(' ')}`);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    console.log('[#] Downloaded package(s)');
+    download();
+};
 
 async function download() {
-    process.stdout.write('[-] Downloading zip file')
-    const axios = require('axios').default
-    const fs = require('node:fs')
+    process.stdout.write('[-] Downloading zip file');
+    const axios = require('axios').default;
+    const fs = require('node:fs');
     const res = await axios({
         url: 'https://api.github.com/repos/HackerShader/HighwayBot/releases',
         method: 'GET',
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
         }
-    })
-    let data = res.data.map(data => data.target_commitish == 'main' ? data : undefined)
+    });
+    let data = res.data.map(data => data.target_commitish == 'main' ? data : undefined);
     let i = 0;
     while (data.indexOf(undefined) != -1) {
         if (data[i] == undefined) data.splice(i, 1)
         else i++
-    }
-    const url = data[0].zipball_url
+    };
+    const url = data[0].zipball_url;
     const zip = await axios({
         url: url,
         method: 'GET',
@@ -40,20 +34,20 @@ async function download() {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
         },
         responseType: 'stream'
-    })
+    });
     zip.data.pipe(fs.createWriteStream('./Highway-Bot.zip'));
     zip.data.once('end', () => {
         process.stdout.clearLine()
         process.stdout.cursorTo(0)
         console.log('[#] Downloaded zip file')
         unzip()
-    })
-}
+    });
+};
 
 async function unzip() {
-    process.stdout.write('[-] Unzipping file')
-    const fs = require('node:fs')
-    const unzipper = require('unzipper')
+    process.stdout.write('[-] Unzipping file');
+    const fs = require('node:fs');
+    const unzipper = require('unzipper');
     fs.createReadStream('./Highway-Bot.zip')
         .pipe(unzipper.Extract({ path: './' }))
         .once('close', () => {
@@ -62,13 +56,13 @@ async function unzip() {
             process.stdout.cursorTo(0)
             console.log('[#] Unzipped file')
             move()
-        })
-}
+        });
+};
 
 async function move() {
-    process.stdout.write('[-] Moving files')
-    const fs = require('fs-extra')
-    const reg = /^HackerShader-HighwayBot-(.+)$/
+    process.stdout.write('[-] Moving files');
+    const fs = require('fs-extra');
+    const reg = /^HackerShader-HighwayBot-(.+)$/;
     let stop = false;
     fs.readdirSync('./')
         .filter(dir => fs.lstatSync(`./${dir}`).isDirectory())
@@ -84,50 +78,51 @@ async function move() {
                 console.log('[#] Moved files')
                 reinstall()
             }
-        })
-}
+        });
+};
 
 async function reinstall() {
-    const packages = Object.keys(require('./package.json').dependencies)
-    process.stdout.write(`[-] Downloading ${packages.length} package(s)`)
-    const child_process = require('child_process')
-    const exce = child_process.exec(yarn == true ? 'yarn' : 'npm install')
+    const packages = Object.keys(require('./package.json').dependencies);
+    process.stdout.write(`[-] Downloading ${packages.length} package(s)`);
+    const child_process = require('child_process');
+    const exce = child_process.exec('npm install');
     exce.on('exit', () => {
-        process.stdout.clearLine()
-        process.stdout.cursorTo(0)
-        console.log('[#] Downloaded package(s)')
-        delete_temp()
-    })
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        console.log('[#] Downloaded package(s)');
+        delete_temp();
+    });
 }
 
 async function delete_temp () {
-    const fs = require('node:fs')
-    if (fs.existsSync('./installer.js')) fs.unlinkSync('./installer.js')
-    if (fs.existsSync('./installer.bat')) fs.unlinkSync('./installer.bat')
-    if (fs.existsSync('./installer.sh')) fs.unlinkSync('./installer.sh')
-    console.log('[#] Removed temporary file.')
-    restart()
-}
+    const fs = require('node:fs');
+    if (fs.existsSync('./installer.js')) fs.unlinkSync('./installer.js');
+    if (fs.existsSync('./installer.bat')) fs.unlinkSync('./installer.bat');
+    if (fs.existsSync('./installer.sh')) fs.unlinkSync('./installer.sh');
+    console.log('[#] Removed temporary file.');
+    restart();
+};
 
 async function restart() {
-    process.stdout.write('[-] Shutt down after 5s')
+    process.stdout.write('[-] Shutt down after 5s');
     setTimeout(() => {
-        process.stdout.clearLine()
-        process.stdout.cursorTo(0)
-        console.log('[#] Shutting down...')
-        process.exit(0)
-    })
-}
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        console.log('[#] Shutting down...');
+        process.exit(0);
+    });
+};
 
-const dns = require('node:dns')
+const dns = require('node:dns');
+const { resolve } = require('path');
 dns.resolve('www.google.com', (err) => {
     if (err) return console.log('[!] You are not connected to the internet.\n[#] Please connect to the internet and try again.')
     else {
         const version = {
             current: /^(.+).(.+).(.+)$/.exec(process.version),
             recommended: ['16', '18']
-        }
-        if (!version.recommended.includes(version.current)) console.log('[!] Please use NodeJS version 18 for best experience.')
-        install()
+        };
+        if (!version.recommended.includes(version.current)) console.log('[!] Please use NodeJS version 18 for best experience.');
+        install();
     }
 })
